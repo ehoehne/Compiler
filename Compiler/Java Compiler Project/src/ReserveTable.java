1f19f92559ef1fc859f0b80adf727cccad729b2c
ehoehne <89826241+ehoehne@users.noreply.github.com>
2023-02-01 22:55:10 -0700

    Added Files

---
+++ Compiler/Java Compiler Project/src/ReserveTable.java
@@ -0,0 +1,31 @@
+public class ReserveTable {
+
+    ReserveTable(int maxSize) {
+
+    }
+
+    int Add(String name, int code) {
+
+        return 0;
+    }
+
+    int LookupName(String name) {
+
+        return 0;
+    }
+
+    String LookupInt(int code) {
+
+        return " ";
+    }
+
+    void PrintReserveTable(String filename) {
+
+    } 
+    
+    void PeePooGrandmaRacoon()
+    {
+
+    }
+    
+}
